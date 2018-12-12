9e0bea5e57404071a2ba4475b65358469f971377
root <root@ip-172-31-44-80.us-east-2.compute.internal>
2018-12-12 05:47:54 +0000

    fix

--- hello.c
+++ hello.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+int fibonnaci(int);
+
 int main(int argc, char **argv){
-	printf("Hello World\n");
+	int i = 0;
+
+	for(i = 0; i <= 30;i++){
+		printf("\nthe value of fibonnaci(%d) is %d",i,fibonnaci(i));
+	}
+}
+
+int fibonnaci(int a){
+//	printf("\n	called fib(%d)",a);
+	if(a <= 1){
+		return a;
+	}
+	return fibonnaci(a -1) + fibonnaci(a - 2);
 }
